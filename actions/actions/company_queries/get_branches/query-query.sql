5c708ff6d55a946f34c4a2481ae6d02341e55f17
UI Bakery <farai.jordan.ctr@ag.ok.gov>
2025-06-27 16:57:24 +0000

    Fix bugs with button functionality on multiple pages

--- actions/actions/company_queries/get_branches/query-query.sql
+++ actions/actions/company_queries/get_branches/query-query.sql
@@ -1,8 +1,10 @@
 select 
 c.company_id, c.name,
-cd.detail_id, cd.license_number, cd.paid_by_headquarter_license, cd.paid_by_headquarter_tonnage, cd.license_payment_made
+cd.detail_id, cd.license_number, cd.paid_by_headquarter_license, cd.paid_by_headquarter_tonnage, cd.license_payment_made, cd.tonnage_payment_made, s.should_report_poundage
 from company c
 left join company_detail cd
 on c.company_id = cd.company_id
+left join seed s
+on s.detail_id = cd.detail_id
 where c.headquarter_id = {{state.company.company_id}}
 or c.company_id = {{state.company.company_id}}
--- pages/company-information/pages/poundage-reporting/actions/get_report_period/code2.js
+++ pages/company-information/pages/poundage-reporting/actions/get_report_period/code2.js
@@ -1,7 +1,6 @@
 	ui.reportPeriod.setValue({{state.report_periods.label}});
 	ui.license_number_select.setValue(state.report_periods.detail_id);
-  {{ui.poundageGoToPayment.setDisabled(true)}};
-  {{ui.addAnotherReportButoon.setDisabled(false)}};
+  {{ui.addAnotherReportButton.setDisabled(false)}};
  {{state.company_stop_sales.shift()}}
 if ({{state.company_stop_sales == 0}}) {
   {{ui.skipReportModal.open()}}
