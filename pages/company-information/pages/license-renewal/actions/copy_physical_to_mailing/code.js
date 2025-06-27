5f18fcb58c35195e4f12423946eee05bcc11d7ff
UI Bakery <farai.jordan.ctr@ag.ok.gov>
2025-06-27 16:10:07 +0000

    Fix duplicate physical to mailing address button

--- pages/company-information/pages/license-renewal/actions/copy_physical_to_mailing/code.js
+++ pages/company-information/pages/license-renewal/actions/copy_physical_to_mailing/code.js
@@ -1,10 +1,7 @@
-let physicalAddress;
-if ({{state.company_addresses.address_type_id == 1}}) {
-  physicalAddress = {{state.company_addresses}}
-}
+let physical_address = {{state.company_addresses.find(v => v.address_type_id == 1)}};
+{{ui.streetAddress.setValue(physical_address.address)}}
+{{ui.addressLine2.setValue(physical_address.address_line_2)}}
+{{ui.city.setValue(physical_address.city)}}
+{{ui.state.setValue(physical_address.state)}}
+{{ui.zip.setValue(physical_address.zip)}}
 
-{{ui.streetAddress.setValue(physicalAddress.address)}};
-{{ui.addressLine2.setValue(physicalAddress.address_line_2)}};
-{{ui.city.setValue(physicalAddress.city)}};
-{{ui.state.setValue(physicalAddress.state)}};
-{{ui.zip.setValue(physicalAddress.zip_code)}};
