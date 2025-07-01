c921f24b4123cddd1c9ccc6eedfc6502d7a4b54d
UI Bakery <farai.jordan.ctr@ag.ok.gov>
2025-07-01 19:34:34 +0000

    remove detail_id

--- actions/actions/app_queries/execute_app_queries/code.js
+++ actions/actions/app_queries/execute_app_queries/code.js
@@ -1,5 +1,4 @@
 {{state.login_information}} = {
 	email: sessionStorage.getItem('login_email'),
- //detail_id: sessionStorage.getItem('detail_id')
-  detail_id: 40480
+ detail_id: sessionStorage.getItem('detail_id')
 }
