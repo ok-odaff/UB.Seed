9b4450991e817b72da0be9d3ff8ed4f28995456c
UI Bakery <farai.jordan.ctr@ag.ok.gov>
2025-07-01 20:14:25 +0000

    fix report license labels

--- pages/company-information/pages/poundage-reporting/actions/get_branch_reports/code.js
+++ pages/company-information/pages/poundage-reporting/actions/get_branch_reports/code.js
@@ -1,6 +1,6 @@
 let options = [];
 for (let branch of {{state.branches}}) {
-if (branch.paid_by_headquarter_tonnage == true && branch.should_report_poundage == true && branch.tonnage_payment_made == false) {
+if (branch.paid_by_headquarter_tonnage == true && branch.should_report_poundage == true && branch.tonnage_payment_made == false || branch.should_report_poundage && branch.headquarter_id == null) {
 options.push({detail_id: branch.detail_id, license_number: branch.license_number})}
 }
      return options
