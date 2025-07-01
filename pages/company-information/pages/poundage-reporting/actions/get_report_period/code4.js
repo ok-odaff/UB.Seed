a82e5b1c9f4a8504d01dffbe7cf8ac4bc910801d
UI Bakery <farai.jordan.ctr@ag.ok.gov>
2025-07-01 20:56:49 +0000

    fix bug on report license select

--- pages/company-information/pages/poundage-reporting/actions/get_report_period/code4.js
+++ pages/company-information/pages/poundage-reporting/actions/get_report_period/code4.js
@@ -1,5 +1,5 @@
 	let report = {{state.branch_reports}}.find(r => r.detail_id == {{state.report_periods.detail_id}})
 {{state.branch_reports}}.shift(report)
-ui.license_number_select.setValue({{state.report_periods.detail_id}})
+{{ui.license_number_select.setValue('')}}
 {{ui.addAnotherReportButton.setDisabled(false)}}
 return {{data}};
--- pages/company-information/pages/poundage-reporting/actions/get_report_period/code6.js
+++ pages/company-information/pages/poundage-reporting/actions/get_report_period/code6.js
@@ -1,6 +1,5 @@
 	let report = {{state.branch_reports}}.find(r => r.detail_id == {{state.report_periods.detail_id}})
 {{state.branch_reports}}.shift(report)
 ui.reportPeriod.setValue({{state.report_periods.report_period}});
-	ui.license_number_select.setValue(state.report_periods.detail_id);
   {{ui.addAnotherReportButton.setDisabled(false)}};
 return {{data}};
