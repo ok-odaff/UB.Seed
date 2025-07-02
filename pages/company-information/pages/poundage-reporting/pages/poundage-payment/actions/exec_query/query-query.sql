50533787591d194c06e3b5fa3fcc1f91082490f3
UI Bakery <farai.jordan.ctr@ag.ok.gov>
2025-07-02 18:53:24 +0000

    check late fees and add description to payment query

--- pages/company-information/pages/poundage-reporting/pages/poundage-payment/actions/exec_query/query-query.sql
+++ pages/company-information/pages/poundage-reporting/pages/poundage-payment/actions/exec_query/query-query.sql
@@ -8,7 +8,7 @@ BEGIN TRY
   
   EXEC sp_executesql @sql,
   @local_vars,
-  {{data.detail_id}}, {{data.payment_type_id}}, {{data.created_date}}, {{data.braintree_id}}, {{data.receipt_id}}, {{data.speedtype_id}}, {{data.fee}}, {{data.late_fee}}, {{data.processing_fee}}, {{data.needs_review}}, {{data.payment_date}}, {{data.created_by}}, {{data.reviewed_by}},  {{data.program_id}}, {{data.company_id}};
+  {{data.detail_id}}, {{data.payment_type_id}}, {{data.created_date}}, {{data.braintree_id}}, {{data.receipt_id}}, {{data.speedtype_id}}, {{data.fee}}, {{data.late_fee}}, {{data.processing_fee}}, {{data.needs_review}}, {{data.payment_date}}, {{data.created_by}}, {{data.reviewed_by}},  {{data.program_id}}, {{data.company_id}}, {{data.description}}, {{data.paid_by_headquarter}};
 
 COMMIT TRANSACTION;
 END TRY
