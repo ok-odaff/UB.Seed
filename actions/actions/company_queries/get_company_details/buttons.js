29853e960aaf0a95c9670bee75a9eae9f6fd4e1a
UI Bakery <farai.jordan.ctr@ag.ok.gov>
2025-07-02 19:53:22 +0000

    change submit functions

--- actions/actions/company_queries/get_company_details/buttons.js
+++ actions/actions/company_queries/get_company_details/buttons.js
@@ -1,6 +1,6 @@
 let reports = 0;
 for (let branch of {{state.branches}}) {
-  if (branch.should_report_poundage == true && branch.tonnage_payment_made == false && branch.paid_by_headquarter_tonnage == true || branch.headquarter_id == null && branch.should_report_poundage == true || branch.headquarter_id != null && branch.paid_by_headquarter_tonnage == false) {
+  if (branch.should_report_poundage == true && branch.tonnage_payment_made == false && branch.paid_by_headquarter_tonnage == true || branch.headquarter_id == null && branch.should_report_poundage == true || branch.headquarter_id == null && branch.paid_by_headquarter_tonnage == false) {
     reports++
   }
 }
