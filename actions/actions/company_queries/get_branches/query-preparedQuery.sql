572da12713385b9ef350a806d8e72e3477eb20c3
UI Bakery <farai.jordan.ctr@ag.ok.gov>
2025-07-03 21:16:49 +0000

    Make changes to improve ui/ux

--- actions/actions/company_queries/get_branches/query-preparedQuery.sql
+++ actions/actions/company_queries/get_branches/query-preparedQuery.sql
@@ -1,7 +1,7 @@
 select 
 c.company_id, c.name, c.headquarter_id,
 cd.detail_id, cd.program_id,  cd.license_number, cd.paid_by_headquarter_license, cd.paid_by_headquarter_tonnage, cd.license_payment_made, cd.tonnage_payment_made, 
-s.should_report_poundage
+s.should_report_poundage, s.exempt_from_license
 from company c
 left join company_detail cd
 on c.company_id = cd.company_id
