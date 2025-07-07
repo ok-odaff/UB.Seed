01e35982de258f9c64d29f42a39813671ba2b230
UI Bakery <farai.jordan.ctr@ag.ok.gov>
2025-07-07 14:30:20 +0000

    Update license calculation to account for multiple branch license types

--- pages/company-information/pages/license-renewal/actions/click_continue/build_line_items.js
+++ pages/company-information/pages/license-renewal/actions/click_continue/build_line_items.js
@@ -1,53 +1,72 @@
+let total_license_fee = 0;
+let total_license_late_fee = 0;
+
+const LICENSE_FEE_RETAIL = Number({{state.program_variables.find(p => p.name == 'LICENSE_FEE_RETAIL').value}});
+const LICENSE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA = Number({{state.program_variables.find(p => p.name == 'LICENSE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA').value}});
+const LICENSE_LATE_FEE_RETAIL = Number({{state.program_variables.find(p => p.name == 'LICENSE_LATE_FEE_RETAIL').value}});
+const LICENSE_LATE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA = Number({{state.program_variables.find(p => p.name == 'LICENSE_LATE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA').value}});
+
 const date = new Date();
-let number_of_licenses;
+
+
 if ({{state.branches.length > 1}}) {
 let licenses = {{state.branches}}.filter(licenses => licenses.paid_by_headquarter_license == true && licenses.license_payment_made == false);
-  console.log(licenses);
-number_of_licenses = licenses.length;
-if ({{state.company.exempt_from_license}} != true) {
-number_of_licenses++
-}  else { number_of_licenses += 0}
-} else { number_of_licenses = 1}
-console.log(number_of_licenses)
-
-const LICENSE_FEE_RETAIL = {{state.program_variables.find(p => p.name == 'LICENSE_FEE_RETAIL').value}};
-const LICENSE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA = {{state.program_variables.find(p => p.name == 'LICENSE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA').value}};
-let LICENSE_FEE;
-
-
-if ({{state.company.company_type == "Retail"}}) {
-  LICENSE_FEE = LICENSE_FEE_RETAIL
-} else {
-  LICENSE_FEE = LICENSE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA
+  for (let license of licenses) {
+  if (license.company_type == 'Retail') {
+  total_license_fee += LICENSE_FEE_RETAIL
+  } else if (license.company_type == 'Wholesale' || license.company_type == 'Medical Marijuana') {
+  total_license_fee += LICENSE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA 
+  }
+  }
 }
 
-const LICENSE_LATE_FEE_RETAIL = {{state.program_variables.find(p => p.name == 'LICENSE_LATE_FEE_RETAIL').value}};
-const LICENSE_LATE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA = {{state.program_variables.find(p => p.name == 'LICENSE_LATE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA').value}};
-let LICENSE_LATE_FEE;
-if ({{state.company.company_type == "Retail"}}) {
-  LICENSE_LATE_FEE = LICENSE_LATE_FEE_RETAIL 
-} else {
-  LICENSE_LATE_FEE = LICENSE_LATE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA
+if ({{state.company.exempt_from_license}} != true) {
+ if (license.company_type == 'Retail') {
+  total_license_fee += LICENSE_FEE_RETAIL
+  } else if (license.company_type == 'Wholesale' || license.company_type == 'Medical Marijuana') {
+  total_license_fee += LICENSE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA 
+  }
 }
 
 const LICENSE_LATE_FEE_DATE = {{state.program_variables.find(p => p.name == 'LICENSE_LATE_FEE_DATE').value}};
-
 const FULL_LATE_FEE_DATE = new Date(LICENSE_LATE_FEE_DATE + '-' + date.getFullYear());
+if (date > FULL_LATE_FEE_DATE || date > new Date(state.company_details.license_expiration_date)) {
+  if ({{state.branches.length > 1}}) {
+let licenses = {{state.branches}}.filter(licenses => licenses.paid_by_headquarter_license == true && licenses.license_payment_made == false);
+  for (let license of licenses) {
+  if (license.company_type == 'Retail') {
+  total_license_late_fee += LICENSE_LATE_FEE_RETAIL
+  } else if (license.company_type == 'Wholesale' || license.company_type == 'Medical Marijuana') {
+  total_license_late_fee += LICENSE_LATE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA 
+  }
+  }
+}
+}
+
+if ({{state.company.exempt_from_license}} != true) {
+ if (license.company_type == 'Retail') {
+  total_license_late_fee += LICENSE_LATE_FEE_RETAIL
+  } else if (license.company_type == 'Wholesale' || license.company_type == 'Medical Marijuana') {
+  total_license_late_fee += LICENSE_LATE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA 
+  }
+}
+
+
 let fees = [];
 
 // Base license fee
 fees.push({
   'description': 'License Fee',
 	'type': 'license_fee',
-  'amount': Number(LICENSE_FEE * number_of_licenses)
+  'amount': total_license_fee
 });
            
 // Add late fee if past date
-if (date > FULL_LATE_FEE_DATE || date > new Date(state.company_details.license_expiration_date)) {
+
   fees.push({
     'description': 'Late Fee',
 		'type': 'license_late_fee',
-    'amount': Number(LICENSE_LATE_FEE * number_of_licenses)
+    'amount': total_license_late_fee
   });
-}
+
 return {{state.fees}} = fees;
