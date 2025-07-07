613c38fdeabdeb8bec985500cea8a9f0dc48d307
UI Bakery <farai.jordan.ctr@ag.ok.gov>
2025-07-07 18:49:46 +0000

    remove late fee temporarily

--- pages/company-information/pages/license-renewal/actions/click_continue/build_line_items.js
+++ pages/company-information/pages/license-renewal/actions/click_continue/build_line_items.js
@@ -28,28 +28,28 @@ if ({{state.company.exempt_from_license}} != true) {
   }
 }
 
-const LICENSE_LATE_FEE_DATE = {{state.program_variables.find(p => p.name == 'LICENSE_LATE_FEE_DATE').value}};
-const FULL_LATE_FEE_DATE = new Date(LICENSE_LATE_FEE_DATE + '-' + date.getFullYear());
-if (date > FULL_LATE_FEE_DATE || date > new Date(state.company_details.license_expiration_date)) {
-  if ({{state.branches.length > 1}}) {
-let licenses = {{state.branches}}.filter(licenses => licenses.paid_by_headquarter_license == true && licenses.license_payment_made == false);
-  for (let license of licenses) {
-  if (license.company_type == 'Retail') {
-  total_license_late_fee += LICENSE_LATE_FEE_RETAIL
-  } else if (license.company_type == 'Wholesale' || license.company_type == 'Medical Marijuana') {
-  total_license_late_fee += LICENSE_LATE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA 
-  }
-  }
-}
-}
+// const LICENSE_LATE_FEE_DATE = {{state.program_variables.find(p => p.name == 'LICENSE_LATE_FEE_DATE').value}};
+// const FULL_LATE_FEE_DATE = new Date(LICENSE_LATE_FEE_DATE + '-' + date.getFullYear());
+// if (date > FULL_LATE_FEE_DATE || date > new Date(state.company_details.license_expiration_date)) {
+//   if ({{state.branches.length > 1}}) {
+// let licenses = {{state.branches}}.filter(licenses => licenses.paid_by_headquarter_license == true && licenses.license_payment_made == false);
+//   for (let license of licenses) {
+//   if (license.company_type == 'Retail') {
+//   total_license_late_fee += LICENSE_LATE_FEE_RETAIL
+//   } else if (license.company_type == 'Wholesale' || license.company_type == 'Medical Marijuana') {
+//   total_license_late_fee += LICENSE_LATE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA 
+//   }
+//   }
+// }
+// }
 
-if ({{state.company.exempt_from_license}} != true) {
- if (state.company.company_type == 'Retail') {
-  total_license_late_fee += LICENSE_LATE_FEE_RETAIL
-  } else if (state.company.company_type == 'Wholesale' || license.company_type == 'Medical Marijuana') {
-  total_license_late_fee += LICENSE_LATE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA 
-  }
-}
+// if ({{state.company.exempt_from_license}} != true) {
+//  if (state.company.company_type == 'Retail') {
+//   total_license_late_fee += LICENSE_LATE_FEE_RETAIL
+//   } else if (state.company.company_type == 'Wholesale' || license.company_type == 'Medical Marijuana') {
+//   total_license_late_fee += LICENSE_LATE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA 
+//   }
+// }
 
 
 let fees = [];
@@ -63,10 +63,10 @@ fees.push({
            
 // Add late fee if past date
 
-  fees.push({
-    'description': 'Late Fee',
-		'type': 'license_late_fee',
-    'amount': total_license_late_fee
-  });
+//   fees.push({
+//     'description': 'Late Fee',
+// 		'type': 'license_late_fee',
+//     'amount': total_license_late_fee
+//   });
 
 return {{state.fees}} = fees;
