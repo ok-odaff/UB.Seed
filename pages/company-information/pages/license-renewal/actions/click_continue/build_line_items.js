139bdbd88722956fbb7065b16a0e7dcd4996ca22
UI Bakery <farai.jordan.ctr@ag.ok.gov>
2025-07-10 20:21:22 +0000

    account for company type casing

--- pages/company-information/pages/license-renewal/actions/click_continue/build_line_items.js
+++ pages/company-information/pages/license-renewal/actions/click_continue/build_line_items.js
@@ -12,22 +12,22 @@ const date = new Date();
 if ({{state.branches.length > 1}}) {
 let licenses = {{state.branches}}.filter(licenses => licenses.paid_by_headquarter_license == true && licenses.license_payment_made == false);
   for (let license of licenses) {
-  if (license.company_type == 'Retail') {
+  if (license.company_type.toLowercase() == 'retail') {
   total_license_fee += LICENSE_FEE_RETAIL
-  } else if (license.company_type == 'Wholesale' || license.company_type == 'Medical Marijuana') {
+  } else if (license.company_type.toLowerCase == 'wholesale' || license.company_type.toLowerCase() == 'marijuana') {
   total_license_fee += LICENSE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA 
   }
   }
 }
 
 if ({{state.company.exempt_from_license}} != true) {
- if (state.company.company_type == 'Retail') {
+ if (state.company.company_type.toLowerCase() == 'retail') {
   total_license_fee += LICENSE_FEE_RETAIL
-  } else if (state.company.company_type == 'Wholesale' || state.company.company_type == 'Medical Marijuana') {
+  } else if (state.company.company_type.toLowerCase() == 'wholesale' || state.company.company_type.toLowerCase() == 'marijuana') {
   total_license_fee += LICENSE_FEE_WHOLESALE_AND_MEDICAL_MARIJUANA 
   }
 }
-
+console.log(total_license_fee)
 // const LICENSE_LATE_FEE_DATE = {{state.program_variables.find(p => p.name == 'LICENSE_LATE_FEE_DATE').value}};
 // const FULL_LATE_FEE_DATE = new Date(LICENSE_LATE_FEE_DATE + '-' + date.getFullYear());
 // if (date > FULL_LATE_FEE_DATE || date > new Date(state.company_details.license_expiration_date)) {
@@ -60,6 +60,8 @@ fees.push({
 	'type': 'license_fee',
   'amount': Number(total_license_fee.toFixed(2))
 });
+  
+  console.log(fees)
            
 // Add late fee if past date
 
