fbe581230cc81b567e085406e022aef05068935d
UI Bakery <farai.jordan.ctr@ag.ok.gov>
2025-07-28 16:13:26 +0000

    allow hq to pay additional licenses after recent payments

--- actions/actions/company_queries/get_company_details/buttons.js
+++ actions/actions/company_queries/get_company_details/buttons.js
@@ -1,10 +1,22 @@
 let reports = 0;
 for (let branch of {{state.branches}}) {
-  if (branch.should_report_poundage == true && branch.tonnage_payment_made == false && branch.paid_by_headquarter_tonnage == true || branch.headquarter_id == null && branch.should_report_poundage == true || branch.headquarter_id == null && branch.paid_by_headquarter_tonnage == false) {
-    reports++
+  if (
+    (branch.should_report_poundage == true && branch.tonnage_payment_made == false && branch.paid_by_headquarter_tonnage == true) ||
+    (branch.headquarter_id == null && branch.should_report_poundage == true) ||
+    (branch.headquarter_id == null && branch.paid_by_headquarter_tonnage == false)
+  ) {
+    reports++;
   }
 }
 if (reports > 0) {
-  {{ui.home_goToPoundage.setDisabled(false)}}
+  {{ui.home_goToPoundage.setDisabled(false)}};
 }
-     return reports
+
+{{ui.home_goToLicense.setDisabled(true)}};
+for (let branch of {{state.branches}}) {
+  if ((branch.paid_by_headquarter_license == true && branch.license_payment_made == false) || (branch.headquarter_id == null && branch.license_payment_made == false && branch.exempt_from_license == false)) {
+    {{ui.home_goToLicense.setDisabled(false)}};
+  }
+}
+
+return reports;
